566a20d325993dd13e9ff29027c191b4d434b093
Junming Zhang <vinalvin1010@gmail.com>
2019-09-11 21:05:02 -0700

    More info about song and movie part works

--- liri.js
+++ liri.js
@@ -2,8 +2,10 @@
 require("dotenv").config();
 var keys = require("./apikey.js");
 
+// Spotify package for songs information
 var Spotify = require('node-spotify-api');
 
+// Axios package
 var axios = require('axios');
 
 var moment = require('moment');
@@ -21,9 +23,27 @@ function spotifyASong(userInput) {
         if (err) {
             console.log("We cannot find this song", err);
         } else {
+            console.log('\n');
             console.log('Artist(s):', data.tracks.items[0].artists[0].name);
+            console.log('Song Name:', data.tracks.items[0].name);
+            console.log('The Link of the Song:', data.tracks.items[0].preview_url);
+            console.log('Album:', data.tracks.items[0].album.name)
+            console.log('\n');
         }
     });
+}
+
+function searchConcert(userInput) {
+
+}
+
+function movieThis(userInput) {
+    axios.get("https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(function(response) {
+        console.log('\n');
+        console.log('Movie Name:', response.data.Title);
+        console.log('The Movie Year:', response.data.Year);
+        console.log('\n');
+    })
 }
 
 if (userCommand === 'spotify-this-song') {
