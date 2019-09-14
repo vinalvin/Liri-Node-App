e9af4cc119ee3c24166c4198397c5cfb5f3bd024
Junming Zhang <vinalvin1010@gmail.com>
2019-09-13 21:54:11 -0700

    fixed the concert bugs

--- liri.js
+++ liri.js
@@ -8,17 +8,19 @@ var Spotify = require('node-spotify-api');
 // Axios package
 var axios = require('axios');
 
+// Date for concert
 var moment = require('moment');
 
 var userCommand = process.argv[2]
 
+// Search, which starting from index 3
 var userInput = process.argv.slice(3).join(" ")
 
 var filesSystem = require("fs")
 
 var spotify = new Spotify(keys.spotify);
 
-function spotifyASong(userInput) {
+function spotifySong(userInput) {
     spotify.search({ type: 'track', query: userInput, limit: 1 }, function(err, data) {
         if (err) {
             console.log("We cannot find this song", err);
@@ -26,7 +28,7 @@ function spotifyASong(userInput) {
             console.log('\n');
             console.log('Artist(s):', data.tracks.items[0].artists[0].name);
             console.log('Song Name:', data.tracks.items[0].name);
-            console.log('The Link of the Song:', data.tracks.items[0].preview_url);
+            console.log('The Song Link:', data.tracks.items[0].preview_url);
             console.log('Album:', data.tracks.items[0].album.name)
             console.log('\n');
         }
@@ -37,53 +39,31 @@ function searchConcert(userInput) {
     axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(function(response) {
             console.log('\n');
             console.log('Venue Name:', response.data[0].venue.name);
-            console.log('Event Date:', moment(response.data[0].datatime).format('MM/DD/YYYY'));
+            console.log('Event Date:', moment(response.data[0].datetime).format('MM/DD/YYYY'));
             console.log('Location:', response.data[0].venue.city);
-            console.log('/n');
+            console.log('\n');
         })
         .catch(function(error) {
             console.log(error);
         })
 }
 
-function movieThis(userInput) {
+function searchMovie(userInput) {
     axios.get("https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(function(response) {
         console.log('\n');
         console.log('Movie Name:', response.data.Title);
         console.log('The Movie Year:', response.data.Year);
+        console.log('Country:', response.data.Country);
+        console.log('Language:', response.data.Language);
+        console.log('Actors', response.data.Actors);
         console.log('\n');
     })
 }
 
-if (userCommand === 'spotify-this-song') {
-    spotifyASong(userInput);
+if (userCommand === 'spotify-song') {
+    spotifySong(userInput);
 } else if (userCommand === 'search-concert') {
     searchConcert(userInput);
-} else if (userCommand === 'movie-this') {
-    movieThis(userInput);
-} else if (userCommand === 'do-what-it-says') {
-    filesSystem.readFile('./random.txt', 'utf8', function(err, data) {
-        console.log(data)
-
-        var dataArr = data.split(",")
-        console.log(data)
-        for (var i = 0; i < dataArr.length; i += 2) {
-            var cmd = dataArr[i]
-            var input = dataArr[i + 1]
-            console.log(cmd)
-            console.log(input)
-            if (cmd === 'spotify-this-song') {
-                spotifyASong(input)
-            } else if (cmd === 'search-concert') {
-                searchConcert(input);
-            } else if (cmd === 'movie-this') {
-                movieThis(input);
-            }
-        }
-    })
+} else if (userCommand === 'search-movie') {
+    searchMovie(userInput);
 }
-
-filesSystem.appendFile('log.txt', userCommand + '\n', function(err) {
-    if (err) throw err;
-    console.log('Showed already');
-});
